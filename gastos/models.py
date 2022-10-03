325cc05f7ad9071865e03c5bb0f04d45b7d3e544
cristianv <cristian.velandia@zinobe.com>
2022-10-03 00:24:35 -0500

    DONE

--- gastos/models.py
+++ gastos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+    balance = models.FloatField(null=True, blank=True, default=False)
+
+    def __str__(self):
+        return {"name": self.name,
+                "balance": self.balance,
+                }
+
+
+class Transaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    amount = models.FloatField(null=True, blank=True, default=None)
+    description = models.CharField(max_length=250)
+    income = models.BooleanField(null=True, blank=True, default=False)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return {self.title
+                }
