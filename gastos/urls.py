9dfd72b00ae41c0cf9b980e842a27c356d74ca34
cristianv <cristian.velandia@zinobe.com>
2022-10-04 15:42:27 -0500

    DONE

--- gastos/urls.py
+++ gastos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import Health
 from .views import AccountView
+from .views import AccountDetailView
 
 urlpatterns = [
     path('', Health.as_view(), name='health'),
-    path('crear_cuenta/', AccountView.as_view())
+    path('crear_cuenta/', AccountView.as_view()),
+    path('detalle_cuenta/<int:id>', AccountDetailView.as_view())
 ]
